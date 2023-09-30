0c51490426226b4f14cf816b9a845c51c79acfc5
valenadels <v.adelsflugel@despegar.com>
2023-09-30 11:11:41 -0300

    del file

--- pkt-loss.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/valentinaadelsflugel/go/pkg/mod/cache/download/github.com/tylertreat/comcast/@v/v1.0.1/github.com/tylertreat/comcast@v1.0.1
-go build comcast.go
-go run comcast.go --device=lo --packet-loss=10%  #if this doesnt work, search the device using: $ ip a
